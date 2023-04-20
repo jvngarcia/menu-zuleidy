d9a314b856314250324cf9d547017ec0cab719c8
jvngarcia <angelgarciaweb@gmail.com>
2023-04-19 21:07:34 -0400

    🚧 update landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,23 +7,23 @@ export default function Home() {
 
       <section>
         <div className="w-full min-h-[450px]">
-          
+
         </div>
-        
+
       </section>
 
 
       <section className="my-12 pt-12 container mx-auto relative">
-        <Image src="/small-tomato.png" width={120} height={120} alt="small tomato" className="absolute top-10 left-6 blur-sm rotate-[-45deg] -z-10" />
-        <Image src="/small-tomato.png" width={120} height={120} alt="small tomato" className="absolute top-16 right-14 rotate-[20deg] blur-sm -z-10" />
-        <Image src="/small-tomato.png" width={80} height={80} alt="small tomato" className="absolute top-28 right-52 rotate-[20deg] blur-sm -z-10" />
-        <Image src="/small-tomato.png" width={150} height={150} alt="small tomato" className="absolute top-40 right-24 rotate-[20deg] drop-shadow -z-10" />
+        <Image src="/small-tomato-2.png" width={120} height={120} alt="small tomato" className="absolute md:top-10 md:left-6 top-[-20px] left-[-20px] blur-sm rotate-[-45deg] -z-10" />
+        <Image src="/small-tomato.png" width={120} height={120} alt="small tomato" className="absolute top-16 md:right-14 right-[-50px] rotate-[20deg] blur-sm -z-10 md:visible invisible" />
+        <Image src="/small-tomato.png" width={80} height={80} alt="small tomato" className="absolute top-28 right-52 rotate-[20deg] blur-sm -z-10 md:visible invisible" />
+        <Image src="/small-tomato.png" width={150} height={150} alt="small tomato" className="absolute top-40 md:right-24 right-[-100px] rotate-[20deg] drop-shadow -z-10" />
         <Image src="/hinojo.png" width={150} height={150} alt="hinojo" className="absolute top-96 left-[-120px] rotate-[50deg] drop-shadow -z-10" />
-        <Image src="/hinojo.png" width={150} height={150} alt="hinojo" className="absolute bottom-0 right-[-120px] rotate-[-50deg] drop-shadow -z-10" />
-        <h1 className="uppercase text-5xl font-semibold text-center">Nevada</h1>
+        <Image src="/hinojo.png" width={150} height={150} alt="hinojo" className="absolute bottom-0 md:right-[-120px] right-[-60px] rotate-[-50deg] drop-shadow -z-10" />
+        <h1 className="uppercase text-4xl font-semibold text-center">Tentación del mar con texturas de la tierra</h1>
         <h6 className="uppercase text-lg tracking-[.25em] mt-4 text-center">By Zuleidy Castillo</h6>
         <div className="flex justify-center text-center">
-          <p className="mt-4 w-1/2">
+          <p className="mt-4 md:w-1/2 md:px-0 px-6">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
             cillum dolore eu fugiat nulla pariatur.
@@ -31,69 +31,52 @@ export default function Home() {
         </div>
 
         <div className="grid md:grid-cols-2 mt-12">
-          <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center md:px-0 px-6">
             <div>
               <h2 className="uppercase text-4xl font-semibold text-center">Preparacion</h2>
               <div className="flex justify-center w-full">
-                <Image src="/separator.png" width={150} height={20} alt="separator" />
+                <Image src="/main-separator.png" width={200} height={20} alt="separator" className="-mt-6 -mb-6" />
               </div>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur.
+                Los botones con número en la receta indican los ingredientes y la forma de preparación de cada elemento del platillo. Cada número corresponde a una acción específica 
+                que se debe realizar en el orden indicado para obtener el resultado final deseado. Siguiendo los circulos numerados, podrás cocinar el plato 
+                de manera organizada y sin olvidar ningún paso importante.
               </p>
+
+              <div className="grid grid-cols-5 mt-8">
+                <div className="flex items-center justify-center">
+                  <button className="bg-slate-600 rounded-full h-8 w-8 text-white flex justify-center items-center opacity-50 hover:opacity-100 transition-all">1</button>
+                </div>
+
+                <div className="flex items-center justify-center">
+                  <button className="bg-slate-600 rounded-full h-8 w-8 text-white flex justify-center items-center opacity-50 hover:opacity-100 transition-all">2</button>
+                </div>
+
+                <div className="flex items-center justify-center">
+                  <button className="bg-slate-600 rounded-full h-8 w-8 text-white flex justify-center items-center opacity-50 hover:opacity-100 transition-all">3</button>
+                </div>
+
+                <div className="flex items-center justify-center">
+                  <button className="bg-slate-600 rounded-full h-8 w-8 text-white flex justify-center items-center opacity-50 hover:opacity-100 transition-all">4</button>
+                </div>
+
+                <div className="flex items-center justify-center">
+                  <button className="bg-slate-600 rounded-full h-8 w-8 text-white flex justify-center items-center opacity-50 hover:opacity-100 transition-all">5</button>
+                </div>
+
+              </div>
             </div>
           </div>
 
           <div className="flex justify-center items-center relative">
-            <Image src="/almeja.png" width={100} height={100} alt="small tomato" className="absolute top-36 right-28 rotate-[20deg] drop-shadow -z-10 blur-sm" />
-            <Image src="/almeja.png" width={200} height={200} alt="small tomato" className="absolute top-24 left-16 rotate-[-80deg] drop-shadow -z-10 blur-[1px]" />
-            <Image src="/platillo.png" width={600} height={600} alt="Platillo por zuleidy castillo" />
-          </div>
-
-
-          <div className="flex justify-center items-center relative">
-            <Image src="/almeja.png" width={100} height={100} alt="small tomato" className="absolute top-36 right-28 rotate-[20deg] drop-shadow -z-10 blur-sm" />
-            <Image src="/almeja.png" width={200} height={200} alt="small tomato" className="absolute top-24 left-16 rotate-[-80deg] drop-shadow -z-10 blur-[1px]" />
-            <Image src="/platillo.png" width={600} height={600} alt="Platillo por zuleidy castillo" />
-          </div>
-          <div className="flex justify-center items-center">
-            <div>
-              <h2 className="uppercase text-4xl font-semibold text-center">Preparacion</h2>
-              <div className="flex justify-center w-full">
-                <Image src="/separator.png" width={150} height={20} alt="separator" />
-              </div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur.
-              </p>
+            <Image src="/almeja.png" width={100} height={100} alt="small tomato" className="absolute md:top-36 md:right-28 top-[50px] right-[20px] rotate-[20deg] drop-shadow -z-10 blur-sm" />
+            <Image src="/almeja.png" width={200} height={200} alt="small tomato" className="absolute md:top-28 md:left-16 top-[30px] left-0 md:rotate-[-40deg] rotate-[-20deg] drop-shadow -z-10 blur-[1px]" />
+            <div className="relative">
+              <Image src="/platillo-principal.png" width={600} height={600} alt="Platillo por zuleidy castillo" />
             </div>
           </div>
         </div>
       </section>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      eneldo
-      almejas
-      arena comestible
-      falso caviar de tomate amarillo
     </main>
   )
 }
